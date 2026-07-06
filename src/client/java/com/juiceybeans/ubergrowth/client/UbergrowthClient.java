b07be662e5f6616ee20fc33f9cb58f163e5b2601
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-07-06 06:05:26 +0100

    Set all blocks' render to cutout

--- src/client/java/com/juiceybeans/ubergrowth/client/UbergrowthClient.java
+++ src/client/java/com/juiceybeans/ubergrowth/client/UbergrowthClient.java
@@ -1,10 +1,16 @@
 package com.juiceybeans.ubergrowth.client;
 
+import com.juiceybeans.ubergrowth.init.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
 
 public class UbergrowthClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        for (Block block : ModBlocks.blocks) {
+            BlockRenderLayerMap.INSTANCE.putBlock(block, RenderType.cutout());
+        }
 	}
 }
