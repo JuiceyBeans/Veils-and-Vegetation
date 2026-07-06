4ca7259227439bc32a4baff7283296d4f2f81fb2
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-07-07 00:18:40 +0100

    Change item models for rust, dust, grime

--- src/client/java/com/juiceybeans/ubergrowth/client/datagen/UbergrowthModelGenerator.java
+++ src/client/java/com/juiceybeans/ubergrowth/client/datagen/UbergrowthModelGenerator.java
@@ -2,17 +2,24 @@ package com.juiceybeans.ubergrowth.client.datagen;
 
 import com.juiceybeans.ubergrowth.Ubergrowth;
 import com.juiceybeans.ubergrowth.init.UbergrowthBlocks;
+import com.mojang.datafixers.util.Pair;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.Util;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.ItemModelGenerators;
-import net.minecraft.data.models.model.ModelTemplate;
-import net.minecraft.data.models.model.TextureMapping;
-import net.minecraft.data.models.model.TextureSlot;
+import net.minecraft.data.models.blockstates.Condition;
+import net.minecraft.data.models.blockstates.MultiPartGenerator;
+import net.minecraft.data.models.blockstates.Variant;
+import net.minecraft.data.models.model.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
 
 public class UbergrowthModelGenerator extends FabricModelProvider {
     private static final ModelTemplate MULTIFACE_TEMPLATE = new ModelTemplate(
@@ -27,8 +34,10 @@ public class UbergrowthModelGenerator extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators generator) {
+        Set<Block> excluded = Set.of(UbergrowthBlocks.RUST, UbergrowthBlocks.DUST, UbergrowthBlocks.GRIME);
+
         for (Block block : UbergrowthBlocks.blocks) {
-            generator.createMultiface(block);
+            this.createMultiface(generator, block, !excluded.contains(block));
             MULTIFACE_TEMPLATE.create(block, new TextureMapping().put(TextureSlot.TEXTURE,
                     Ubergrowth.id("block/" + BuiltInRegistries.BLOCK.getKey(block).getPath())), generator.modelOutput);
         }
@@ -36,5 +45,33 @@ public class UbergrowthModelGenerator extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerators generator) {
+        generator.generateFlatItem(UbergrowthBlocks.RUST.asItem(), ModelTemplates.FLAT_ITEM);
+        generator.generateFlatItem(UbergrowthBlocks.DUST.asItem(), ModelTemplates.FLAT_ITEM);
+        generator.generateFlatItem(UbergrowthBlocks.GRIME.asItem(), ModelTemplates.FLAT_ITEM);
+    }
+
+    public final void createMultiface(BlockModelGenerators generator, Block multifaceBlock, boolean generateItemModel) {
+        if (generateItemModel) generator.createSimpleFlatItemModel(multifaceBlock);
+
+        ResourceLocation resourceLocation = ModelLocationUtils.getModelLocation(multifaceBlock);
+        MultiPartGenerator multiPartGenerator = MultiPartGenerator.multiPart(multifaceBlock);
+        Condition.TerminalCondition terminalCondition = Util.make(
+                Condition.condition(), terminalConditionx -> BlockModelGenerators.MULTIFACE_GENERATOR.stream().map(Pair::getFirst).forEach(booleanPropertyx -> {
+                    if (multifaceBlock.defaultBlockState().hasProperty(booleanPropertyx)) {
+                        terminalConditionx.term(booleanPropertyx, false);
+                    }
+                })
+        );
+
+        for (Pair<BooleanProperty, Function<ResourceLocation, Variant>> pair : BlockModelGenerators.MULTIFACE_GENERATOR) {
+            BooleanProperty booleanProperty = pair.getFirst();
+            Function<ResourceLocation, Variant> function = pair.getSecond();
+            if (multifaceBlock.defaultBlockState().hasProperty(booleanProperty)) {
+                multiPartGenerator.with(Condition.condition().term(booleanProperty, true), function.apply(resourceLocation));
+                multiPartGenerator.with(terminalCondition, function.apply(resourceLocation));
+            }
+        }
+
+        generator.blockStateOutput.accept(multiPartGenerator);
     }
 }
