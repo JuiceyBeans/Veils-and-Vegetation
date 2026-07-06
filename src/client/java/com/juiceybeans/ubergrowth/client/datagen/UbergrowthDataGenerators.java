487f3948df71f82581d6458f88b591a65e3fb9c0
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-07-06 06:50:15 +0100

    DataGenerator -> DataGenerators

--- src/client/java/com/juiceybeans/ubergrowth/client/datagen/UbergrowthDataGenerators.java
+++ src/client/java/com/juiceybeans/ubergrowth/client/datagen/UbergrowthDataGenerators.java
@@ -3,7 +3,7 @@ package com.juiceybeans.ubergrowth.client.datagen;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class UbergrowthDataGenerator implements DataGeneratorEntrypoint {
+public class UbergrowthDataGenerators implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
